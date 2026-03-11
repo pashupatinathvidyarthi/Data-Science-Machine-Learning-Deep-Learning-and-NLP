a5ed8484cd1e6126a406049078be7f0bb2bb8aca
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-03-12 01:29:34 +0530

    HTML

---
+++ Python/15_Getting Started_with_Flask_Framework/flask/getpost.py
@@ -0,0 +1,35 @@
+from flask import Flask,render_template,request
+
+"""
+It creates an instances of the Flask class,
+which will be your WSGI(Web Server Gateway Interface) application.
+"""
+##WSGI Application
+app=Flask(__name__)
+
+@app.route("/") ## / means home page of web page
+def welcome():
+    return "<html><h1>Welcome to the Flask</h1></html"
+
+@app.route("/index",methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route('/form',methods=['GET','POST'])
+def form():
+    if request.method=='POST':
+        pass
+    return render_template('form.html')
+
+if __name__=="__main__":        ##This is entry of any .py file
+    app.run(debug=True)   ## debuge=True means website getting update dynamically as we made chnage in the file
+
+
+"""
+To redirect to the paticular html file we need to create templates file inside 
+flask and inside templates file html file name should be 'index.html' 
+"""   
