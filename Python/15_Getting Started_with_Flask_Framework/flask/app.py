337c8bfec54f1bdcd62ab4994ce36a95d5dabb97
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-03-12 00:53:55 +0530

    Python

--- Python/15_Getting Started_with_Flask_Framework/flask/app.py
+++ Python/15_Getting Started_with_Flask_Framework/flask/app.py
@@ -4,7 +4,12 @@ from flask import Flask
 It creates an instances of the Flask class,
 which will be your WSGI(Web Server Gateway Interface) application.
 """
-app=Flask()
+##WSGI Application
+app=Flask(__name__)
 
+@app.route("/") ## / means home page of web page
+def welcome():
+    return "Welcome to this Falsk.This should be amazing course"
 
-if __name__=="__main__":     ##This is entry of any .py file
+if __name__=="__main__":        ##This is entry of any .py file
+    app.run(debug=True)     
