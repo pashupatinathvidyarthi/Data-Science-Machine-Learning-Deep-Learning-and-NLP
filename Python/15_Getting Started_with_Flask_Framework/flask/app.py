83f461b5d1ced906bc03a5c5a03e9301dd535558
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-03-12 00:58:17 +0530

    Flask

--- Python/15_Getting Started_with_Flask_Framework/flask/app.py
+++ Python/15_Getting Started_with_Flask_Framework/flask/app.py
@@ -9,7 +9,11 @@ app=Flask(__name__)
 
 @app.route("/") ## / means home page of web page
 def welcome():
-    return "Welcome to this Falsk.This should be amazing course"
+    return "Welcome to this best Falsk.This should be amazing course"
+
+@app.route("/index")
+def index():
+    return "Welcome to the index page"
 
 if __name__=="__main__":        ##This is entry of any .py file
-    app.run(debug=True)     
+    app.run(debug=True)   ## debuge=True means website getting update dynamically as we made chnage in the file   
