dcbd535a27db33016a767f145bf68ad74a874ebc
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-03-12 00:45:54 +0530

    Python

---
+++ Python/15_Getting Started_with_Flask_Framework/flask/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+"""
+It creates an instances of the Flask class,
+which will be your WSGI(Web Server Gateway Interface) application.
+"""
+app=Flask()
+
+
+if __name__=="__main__":     ##This is entry of any .py file
