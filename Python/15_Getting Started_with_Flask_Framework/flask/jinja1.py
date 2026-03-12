3ec849857d85afa73f2ad84c59246605c7a5b6a0
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-03-12 16:21:05 +0530

    Flask

---
+++ Python/15_Getting Started_with_Flask_Framework/flask/jinja1.py
@@ -0,0 +1,44 @@
+## Building URL Dynamically
+## Variable Rule
+## Jinja 2 Template Engine
+
+
+from flask import Flask,render_template,request
+
+"""
+It creates an instances of the Flask class,
+which will be your WSGI(Web Server Gateway Interface) application.
+"""
+##WSGI Application
+app=Flask(__name__)
+
+@app.route("/") ## / means home page of web page
+def welcome():
+    return "<html><h1>Welcome to the Flask</h1></html"
+
+@app.route("/index",methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route('/submit',methods=['GET','POST'])
+def submit():
+    if request.method=='POST':
+        name=request.form['name']
+        return f"Hello {name}!"
+    return render_template('form.html')
+
+## Varibale Rule
+@app.route('/success/<int:score>')
+def success(score):
+    res=""
+    if score>=50:
+        res="PASS"
+    else:
+        res="FAIL"
+
+if __name__=="__main__":      
+    app.run(debug=True)   
