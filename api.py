39d526a36ef83d3d643da69ef5bb032d33f520be
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-03-13 15:51:53 +0530

    Flask

--- api.py
+++ api.py
@@ -12,6 +12,16 @@ items={
     {"id":2, "name":"Item 2", "description":"This is item 2"}
 }
 
+@app.route('/')
+def home():
+    return "Welcome To The Sample To Do List App"
+
+
+## Get : Retrieve all the items
+@app.route('/items',methods=['GET'])
+def get_items():
+    return jsonify(items)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
