0ce4b32ab5b69073c8e0ddc3f16c7a9e8c352f63
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-03-13 15:48:33 +0530

    Flask

--- api.py
+++ api.py
@@ -1,2 +1,17 @@
 ### Put and Delete
-### Working with API's
+### Working with API's --- json
+
+
+from flask import Flask, jsonify, request
+
+app=Flask(__name__)
+ 
+##Initial Data in my to do List
+items={
+    {"id":1, "name":"Item 1", "description":"This is item 1"},
+    {"id":2, "name":"Item 2", "description":"This is item 2"}
+}
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
