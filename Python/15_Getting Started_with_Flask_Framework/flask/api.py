f36df13a0323712b23d8b4bc0b695d30dd09b273
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-03-13 16:28:49 +0530

    Flask

--- Python/15_Getting Started_with_Flask_Framework/flask/api.py
+++ Python/15_Getting Started_with_Flask_Framework/flask/api.py
@@ -7,10 +7,10 @@ from flask import Flask, jsonify, request
 app=Flask(__name__)
  
 ##Initial Data in my to do List
-items={
+items=[
     {"id":1, "name":"Item 1", "description":"This is item 1"},
     {"id":2, "name":"Item 2", "description":"This is item 2"}
-}
+]
 
 @app.route('/')
 def home():
