02a02344a6a9ae00fe9be85c1ada0b4fcad637fd
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-03-15 01:35:14 +0530

    Streamlit

--- Python/16_Getting Started_with_Streamlit_Web_Framework/classification.py
+++ Python/16_Getting Started_with_Streamlit_Web_Framework/classification.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
 
-@st.cache
+@st.cache_data
 def load_data():
     iris=load_iris()
     df=pd.DataFrame(iris.data,columns=iris.feature_names)
@@ -15,8 +15,18 @@ df,target_name=load_data()
 model=RandomForestClassifier()
 model.fit(df.iloc[:,:-1],df['species'])
 
-st.slider.title("Input Features")
+st.sidebar.title("Input Features")
 sepal_length=st.sidebar.slider("Sepal Length", float(df['sepal length (cm)'].min()),float(df['sepal length (cm)'].max()))
 sepal_width=st.sidebar.slider("Sepal Width", float(df['sepal width (cm)'].min()),float(df['sepal width (cm)'].max()))
 petal_length=st.sidebar.slider("Sepal Length", float(df['petal length (cm)'].min()),float(df['petal length (cm)'].max()))
 petal_width=st.sidebar.slider("Sepal Length", float(df['petal width (cm)'].min()),float(df['petal width (cm)'].max()))
+
+
+input_data=[[sepal_length,sepal_width,petal_length,petal_width]]
+
+##Prediction
+prediction=model.predict(input_data)
+predicted_species=target_name[prediction[0]]
+
+st.write("Prediction")
+st.write(f"The predicted species is : {predicted_species}")
