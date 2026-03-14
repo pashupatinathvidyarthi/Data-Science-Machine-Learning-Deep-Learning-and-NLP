c536a0e9f773ab0350eac9042b9d060221c53b60
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-03-15 01:06:16 +0530

    Streamlit

--- Python/16_Getting Started_with_Streamlit_Web_Framework/widgets.py
+++ Python/16_Getting Started_with_Streamlit_Web_Framework/widgets.py
@@ -24,4 +24,14 @@ data={
 }
 
 df=pd.DataFrame(data)
+##To save above dataframe in folder
+df.to_csv("sampledata.csv")
 st.write(df)
+
+
+## Upload File
+uploaded_file=st.file_uploader("Choose a CSV file",type="csv")
+
+if uploaded_file is not None:
+    df=pd.read_csv(uploaded_file)
+    st.write(df)
