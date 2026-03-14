bfac5240eff45d37f2d7be896e43b864286d8663
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-03-15 01:23:02 +0530

    Streamlit

--- Python/16_Getting Started_with_Streamlit_Web_Framework/classification.py
+++ Python/16_Getting Started_with_Streamlit_Web_Framework/classification.py
@@ -3,3 +3,19 @@ import pandas as pd
 from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
 
+@st.cache
+def load_data():
+    iris=load_iris()
+    df=pd.DataFrame(iris.data,columns=iris.feature_names)
+    df['species']=iris.target
+    return df,iris.target_names
+
+df,target_name=load_data()
+
+model=RandomForestClassifier()
+model.fit(df.iloc[:,:-1],df['species'])
+
+st.slider.title("Input Features")
+sepal_length=st.sidebar.slider("Sepal Length", float(df['sepal length (cm)'].min()),float)
+sepal_width=st.sidebar.slider("Sepal Width", float(df['sepal width (cm)'].min()),float)
+sepal_length=st.sidebar.slider("Sepal Length", float(df['sepal length (cm)'].min()),float)
