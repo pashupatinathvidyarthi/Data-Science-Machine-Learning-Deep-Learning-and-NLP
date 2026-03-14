8dfb2a7750f376b070b7bb92a53c38b938f437b2
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-03-15 00:59:48 +0530

    Streamlit

--- Python/16_Getting Started_with_Streamlit_Web_Framework/widgets.py
+++ Python/16_Getting Started_with_Streamlit_Web_Framework/widgets.py
@@ -7,5 +7,9 @@ name=st.text_input("Enter your name : ")
 age=st.slider("Select your age : ",0,100,25) ##Slider (start,stop,initial value)
 st.write(f"Your age is {age}.")
 
+options=["Python","Java","C++","JavaScript"]
+choice=st.selectbox("Choose your favourite language : ",options)
+st.write(f"You selected {choice}")
+
 if name:
     st.write(f"Hello, {name}")
