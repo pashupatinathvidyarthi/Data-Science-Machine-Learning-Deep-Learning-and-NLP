97ad915fe39235be2f04b988fc792bdb417b979f
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-03-15 00:56:40 +0530

    Streamlit

--- Python/16_Getting Started_with_Streamlit_Web_Framework/widgets.py
+++ Python/16_Getting Started_with_Streamlit_Web_Framework/widgets.py
@@ -4,5 +4,8 @@ st.title("Streamlit Text Input")
 
 name=st.text_input("Enter your name : ")
 
+age=st.slider("Select your age : ",0,100)
+st.write(f"Your age is {age}.")
+
 if name:
     st.write(f"Hello, {name}")
